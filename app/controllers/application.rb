d63d6a10b30fd4abbb05a6c3026a00ef132a6aae
Mikal Priess <mikal@mikalp.com>
2008-08-17 16:40:31 -0700

    After one small change to learn git

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,6 +1,9 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
+# one small change to see how git handles it
+# mp 8/16/2008
+
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
